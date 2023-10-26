6181147e2205824f55e176d8f39bf1ca1a483e3a
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-10-25 21:26:07 -0500

    testing last image routes

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -7,22 +7,22 @@ const Background = () => {
     <>
       <div>
         <img
-          src="/images/image.jpg"
+          src={PUBLIC_DIR + '/assets/images/image.jpg'}
           className="bg__banana"
           alt=""
         />
         <img
-          src="/assets/images/bgPatternLeaf.png"
+          src="assets/images/bgPatternLeaf.png"
           className="bg__leaf"
           alt=""
         />
         <img
-          src="/public/assets/images/bgPatternStrawberry.png"
+          src="public/assets/images/bgPatternStrawberry.png"
           className="bg__berry"
           alt=""
         />
         <img
-          src="/src/assets/images/bgFrameStain.png"
+          src="src/assets/images/bgFrameStain.png"
           className="bg__stain"
           alt=""
         />

24e3075bc8210fbbb4bd379ae0aebc034c7e15dd
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-10-25 19:54:46 -0500

    url update

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -4,10 +4,10 @@ import './Background.css';
 const Background = () => {
   return <>
   <div>
-    <img src="src/assets/bgPattern-banana.png" className="bg__banana" alt="" />
-    <img src="src/assets/bgPattern-leaf.png" className="bg__leaf" alt="" />
-    <img src="src/assets/bgPattern-strawberry.png" className="bg__berry" alt="" />
-    <img src="src/assets/bgFrameStain.png" className="bg__stain"  alt="" />
+    <img src="public/assets/bgPattern-banana.png" className="bg__banana" alt="" />
+    <img src="public/assets/bgPattern-leaf.png" className="bg__leaf" alt="" />
+    <img src="public/assets/bgPattern-strawberry.png" className="bg__berry" alt="" />
+    <img src="public/assets/bgFrameStain.png" className="bg__stain"  alt="" />
   </div>
   </>;
 };

dc5a77dcf5c77a3f36d2a705c1c3936656de91ef
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-10-25 20:09:06 -0500

    testing route image

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -4,9 +4,9 @@ const Background = () => {
   return <>
   <div>
     <img src="" className="bg__banana" alt="" />
-    <img src="../../../assets/bgPattern-leaf.png" className="bg__leaf" alt="" />
-    <img src="../../assets/bgPattern-strawberry.png" className="bg__berry" alt="" />
-    <img src="/assets/bgFrameStain.png" className="bg__stain"  alt="" />
+    <img src="../../../assets/images/bgPattern-leaf.png" className="bg__leaf" alt="" />
+    <img src="../../assets/images/bgPattern-strawberry.png" className="bg__berry" alt="" />
+    <img src="/assets/images/bgFrameStain.png" className="bg__stain"  alt="" />
   </div>
   </>;
 };

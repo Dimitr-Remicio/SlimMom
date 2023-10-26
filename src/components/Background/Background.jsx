245bb571454afc03a115a725d4d0fe7f213beddd
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-10-25 21:04:31 -0500

    url

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -12,17 +12,17 @@ const Background = () => {
           alt=""
         />
         <img
-          src={PUBLIC_DIR + "/assets/images/bgPatternLeaf.png"}
+          src="./src/assets/images/bgPatternLeaf.png"
           className="bg__leaf"
           alt=""
         />
         <img
-          src={PUBLIC_DIR + "/assets/images/bgPatternStrawberry.png"}
+          src="./public/assets/images/bgPatternStrawberry.png"
           className="bg__berry"
           alt=""
         />
         <img
-          src={"/assets/images/bgFrameStain.png"}
+          src="/assets/images/bgFrameStain.png"
           className="bg__stain"
           alt=""
         />

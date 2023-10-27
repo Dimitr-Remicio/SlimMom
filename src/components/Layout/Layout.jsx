2078699c8a604b72c84f32737c87e6b6390a5d19
Cupez04 <selop0639@gmail.com>
2023-10-27 09:32:15 -0500

    banners

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,16 +3,25 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Routers from "../../routers/Routers";
 import Background from "../Background/Background";
+import Prueba from "../Prueba/Prueba";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Background/>
+      {/* <Background/> */}
+      <div className="prueba-container" style={{
+        position: "absolute", /* Usar posición absoluta */
+        bottom: "5rem",
+        right: "0", 
+        zIndex: "2", 
+      }}>
+        <Prueba />
+      </div>
       <div>
         <Routers />
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,82 @@
 import React from "react";
-import Container from "../components/Container/Container";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Buttons from "../components/Buttons/Buttons";
+import Stack from '@mui/material/Stack';
+import "../styles/login.css";
+
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <Container>
-      <h1>holaaaa</h1>
-    </Container>
+    <div
+      style={{
+        marginTop: "120px",
+        marginLeft: "10vw",
+      }}
+    >
+      <h1 style={{
+        color: "#FC842D",
+      }}>Log In</h1>
+    
+        <div>
+          <div style={{ marginBottom: "30px", marginTop: "30px" }}>
+            <TextField
+              color="warning"
+              sx={{ width: "25ch", '& label': { letterSpacing: '1px' } }}
+              id="standard-basic"
+              label="Email*"
+              variant="standard"
+            />
+          </div>
+          <FormControl
+            color="warning"
+            sx={{ width: "25ch" }}
+            variant="standard"
+          >
+            <InputLabel className="input__login" htmlFor="standard-adornment-password" style={{ letterSpacing: '1px' }}>
+              Password*
+            </InputLabel>
+            <Input
+              color="warning"
+              focused
+              id="standard-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </div>
+      <section>
+      <Stack direction="row" spacing={2}>
+        <button className="login__buttons">Login</button>
+        <button className="login__buttons">Create <br /> An Acount</button>      
+      </Stack>
+
+      </section>
+    </div>
   );
 };
 

b6f7a6c7d13915329303e32156750241af14aeb5
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-10-27 14:03:25 -0500

    add Components to Home

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -11,10 +11,19 @@ const Layout = () => {
       <Header />
       <div className="Layout">
         <Container>
-          <div className="blur"></div>
           <BackAnimated />
-          {/* <Background /> */}
-          <Routers />
+          <div
+            style={{
+              position: "relative",
+              zIndex: "10",
+              backgroundColor: "#FFFFFF",
+              width: "auto",
+              background:
+                "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 5%, rgba(255,255,255,1) 15%, rgba(255,255,255,1) 45%, rgba(255,255,255,0) 70%)",
+            }}
+          >
+            <Routers />
+          </div>
         </Container>
       </div>
     </>

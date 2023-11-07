a07b0fb2e0b242ed0a1eae7c1a2902cb41e276e5
Dimitr-remicio <dimitr.darm27@gmail.com>
2023-11-07 13:40:05 -0500

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,39 +5,34 @@ import calcSelectors from "../../redux/calculatorSlice/calculatorSelectors";
 import { useDispatch, useSelector } from "react-redux";
 import { wipeUser } from "../../redux/calculatorSlice/calcSlice";
 
+import "./modal.css";
+
 const Modal = ({ isOpen, onClose }) => {
   const { dailyRate, notHealthy } = useSelector(calcSelectors.getUserData);
   console.log(dailyRate);
   console.log(notHealthy);
 
   const dispatch = useDispatch();
-  const modalStyles = {
-    width: "672px",
-    height: "574px",
-    position: "fixed",
-    top: "50%", // Centra verticalmente
-    left: "50%", // Centra horizontalmente
-    transform: "translate(15%, -60%)", // Centra tanto horizontal como verticalmente
-    border: "1px solid #E0E0E0",
-    background: "white",
-    zIndex: 9999, // Asegura que el modal esté por encima de otros elementos
-  };
+
 
   const titleStyles = {
     fontFamily: "Verdana",
-    fontSize: "26px",
+    fontSize: "1em",
     fontWeight: 700,
     lineHeight: "36px",
     letterSpacing: "0em",
     textAlign: "center",
-    marginTop: "100px",
+    marginTop: "50px",
   };
   const close = {
     position: "absolute",
-    top: "10px", // Ajusta la posición vertical
-    right: "10px", // Ajusta la posición horizontal para la esquina derecha
+    width:"10px",
+    fontSize:"25px",
+    top: "2rem", // Ajusta la posición vertical
+    right: "2rem", // Ajusta la posición horizontal para la esquina derecha
     cursor: "pointer",
     color: "black",
+
   };
   const valuekcal = {
     fontFamily: "Verdana",
@@ -60,12 +55,11 @@ const Modal = ({ isOpen, onClose }) => {
     marginTop: "30px",
   };
   const vectorStyles = {
-    width: "330px",
+    minWidth: "70px",
+    width: "100%",
+    maxWidth: "330px",
     height: "1px", // Altura de 1 píxel para que parezca una línea
-    top: "300px",
-    left: "184px",
     border: "1px solid #E0E0E0", // Border de 1 píxel sólido de color negro
-    position: "absolute",
   };
 
   const notfoot = {
@@ -75,50 +69,72 @@ const Modal = ({ isOpen, onClose }) => {
     lineHeight: "17px",
     letterSpacing: "0.04em",
     textAlign: "center",
-    marginTop: "100px",
+    marginTop: "40px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
   };
   const buttonStar = {
     backgroundColor: "#FC842D",
     padding: "15px 20px",
-    width: "18rem",
+    minWidth: "8rem",
+    width: "100%",
+    maxWidth: "18rem",
     borderRadius: "20em",
     fontFamily: "Verdana bold",
     textTransform: "none",
-    position: "relative",
-    top: "146px",
-    left: "194px",
     color: "#FFFFFF",
     borderColor: "transparent",
     cursor: "pointer",
+    position: "relative",
+    
+    // bottom:"-8rem"
+  };
+  const listnotfoot = {
+    fontFamily: "Verdana",
+    fontFize: "14px",
+    fontWeight: "400",
+    lineHeight: "17px",
+    letterSpacing: "0.04em",
+
+    color: "#9B9FAA",
+    marginTop: "20px",
+    textAlign: "center",
   };
 
   return (
     isOpen && (
-      <div>
-        <div style={modalStyles}>
-          {/* Contenido del modal */}
-          <Button
-            style={close}
-            onClick={() => {
-              onClose();
-              dispatch(wipeUser());
-            }}
-          >
-            X
-          </Button>
-          <h2 style={titleStyles}>Your recommended daily calorie intake is</h2>
-          <br />
-          <p style={valuekcal}>
-            2800 <span style={kcla}>kcal</span>
-          </p>
-          <p style={vectorStyles}></p>
-          <br />
-          <p style={notfoot}>Foods you shouldnt eat</p>
-          <ul></ul>
+      <div className="backdrop">
+          <div className="modAl">
+            <div className="contModal">
+            <Button
+              style={close}
+              onClick={() => {
+                onClose();
+                dispatch(wipeUser());
+              }}
+            >
+              X
+            </Button>
+              <h2 style={titleStyles}>
+                Your recommended daily calorie intake is
+              </h2>
+              <br />
+              <p style={valuekcal}>
+                {dailyRate}<span style={kcla}>kcal</span>
+              </p>
+              <p style={vectorStyles}></p>
+              <p style={notfoot}>Foods you shouldnt eat</p>
+              <ul style={listnotfoot}>
+                {notHealthy.map((food, index) => (
+                  <li key={index}> • {food}</li>
+                ))}
+              </ul>
 
-          <NavLink to="/SlimMom/signup">
-            <button style={buttonStar}> Start losing weight</button>
-          </NavLink>
+            <NavLink to="/SlimMom/signup">
+              <button style={buttonStar}> Start losing weight</button>
+            </NavLink>
+            </div>
         </div>
       </div>
     )

36a69b615034a5033c09d5e49320166ab953a556
JuanC <Juanhenao123kl@gmail.com>
2023-11-12 15:02:17 -0500

    firts commit

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Calculator from "../pages/Calculator";
@@ -10,57 +11,36 @@ import { useSelector } from "react-redux";
 const Routers = () => {
   const isLoggedIn = useSelector(authSelector.getIsLoggedIn);
 
-  return (
-    <>
-      {/* {userIsLoggedIn ? (
-        <Routes>
-          <Route path="/SlimMom/calculator" element={<Calculator />} />
-          <Route path="/SlimMom/diary" element={<Diary />} />
-        </Routes>
-      ) : ( */}
-      {/* <Routes>
-          <Route path="SlimMom/" element={<Navigate to="home" />} />
-          <Route path="/SlimMom/home" element={<Home />} />
-          <Route path="/SlimMom/login" element={<Login />} />
-          <Route path="/SlimMom/signup" element={<Signup />} />
-        </Routes> */}
-      {/* )} */}
+  useEffect(() => {
+    const lastVisitedRoute = localStorage.getItem("lastVisitedRoute");
+    if (lastVisitedRoute) {
+      window.location.replace(lastVisitedRoute);
+    }
+  }, []);
 
-      <Routes>
-        <Route path="*" element={<Navigate to="/home" />} />
-        <Route path="SlimMom" element={<Navigate to="home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/calculator"
-          element={
-            isLoggedIn ? (
-              <Calculator />
-            ) 
-            : (
-              <Navigate to="/home" />
-              // {/* <Calculator /> */}
-            )
-          }
-        />
-        <Route
-          path="/diary"
-          element={
-            isLoggedIn ? (
-              <Diary />
-            ) : (
-              // <Diary /> 
-              <Navigate to="/home" />
-            )
-          }
-        />
-      </Routes>
-    </>
+  const handleRouteChange = (newPath) => {
+    localStorage.setItem("lastVisitedRoute", newPath);
+  };
+
+  const LoginRedirect = () => {
+    if (isLoggedIn) {
+      handleRouteChange("/SlimMom/diary");
+      return <Navigate to="/SlimMom/diary" replace />;
+    }
+    return <Login />;
+  };
+
+  return (
+    <Routes>
+      <Route path="*" element={<Navigate to="/home" />} />
+      <Route path="SlimMom" element={<Navigate to="home" />} />
+      <Route path="/home" element={<Home />} onNavigate={(newPath) => handleRouteChange(newPath)} />
+      <Route path="/login" element={<LoginRedirect />} />
+      <Route path="/signup" element={<Signup />} onNavigate={(newPath) => handleRouteChange(newPath)} />
+      <Route path="/calculator" element={isLoggedIn ? <Calculator /> : <Navigate to="/home" replace state={{ from: "/calculator" }} />} onNavigate={(newPath) => handleRouteChange(newPath)} />
+      <Route path="/diary" element={isLoggedIn ? <Diary /> : <Navigate to="/home" replace state={{ from: "/diary" }} />} onNavigate={(newPath) => handleRouteChange(newPath)} />
+    </Routes>
   );
 };
-
-// Asdfghjkl12345
-// asdsds@gmail.com
 
 export default Routers;
